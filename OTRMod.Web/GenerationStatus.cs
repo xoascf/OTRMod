a0539334dedabc6327fc7d704d0685f80dc4f19c
Amaro Martínez <xoas@airmail.cc>
2022-10-20 02:19:50 -0500

    Update web

---
+++ OTRMod.Web/GenerationStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace OTRMod.Web;
+
+public enum GenerationStatus
+{
+	NoSelected, // None is selected.
+	Selected,   // At least one is selected.
+	Generating, // Processing (decompressing/creating OTR)
+	Finished,   // Then we're done.
+	Error,      // F.
+}
