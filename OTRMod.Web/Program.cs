0586e82f70c34996567ead151528dacaca51cb74
Amaro Martínez <xoas@airmail.cc>
2023-05-26 11:44:52 -0500

    Add PWAUpdater

--- OTRMod.Web/Program.cs
+++ OTRMod.Web/Program.cs
@@ -1,4 +1,5 @@
 using BlazorDownloadFile;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OTRMod.Web;
@@ -6,6 +7,8 @@ using OTRMod.Web;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddPWAUpdater();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorDownloadFile();
