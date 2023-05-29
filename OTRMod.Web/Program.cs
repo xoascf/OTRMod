90af93c2a579bdb7db53366e9bd5360a8ddc153f
Amaro Martínez <xoas@airmail.cc>
2023-05-28 23:21:01 -0500

    OTRMod.Web: Add basic translation support

--- OTRMod.Web/Program.cs
+++ OTRMod.Web/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddPWAUpdater();
+builder.Services.AddLocalization();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorDownloadFile();
