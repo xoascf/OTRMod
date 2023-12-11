37c8b43aaa5da270595758be76f83550b08beabf
Amaro Martínez <xoas@airmail.cc>
2023-12-11 12:59:52 -0500

    Add text export from .h message files

--- OTRMod/ID/Text.cs
+++ OTRMod/ID/Text.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace OTRMod.ID;
 
 public static class Text {
@@ -54,5 +57,118 @@ public static class Text {
 		public TextBoxPosition BoxPos;
 		public int Offset;
 		public List<byte>? Content;
+	}
+
+	public static char C(this string hex) => (char)Convert.ToInt32(hex, 16);
+
+	public static char C<TEnum>(string value) where TEnum : struct, Enum {
+		return Enum.TryParse(value, out TEnum res)
+			? (char)Convert.ToInt32(res)
+			: throw new ArgumentException("Invalid hex", nameof(value));
+	}
+
+	public static string EvalCodes(string input) {
+		StringBuilder sb = new(); bool inQuotes = false;
+		string hex = @"\\x([0-9A-Fa-f]{2})";
+		input = input.Replace("\\\"", "\u201C"); // Unescape quotes by replacing them
+		string codes = $@"^(?<code>{string.Join("|", Enum.GetNames(typeof(Code)))})\b";
+		for (int i = 0; i < input.Length; i++) {
+			if (input[i] == '\"') { inQuotes = !inQuotes; continue; }
+			if (inQuotes) sb.Append(input[i]);
+			else {
+				string s = input[i..]; Match m; string cn;
+				if ((m = Regex.Match(s, @"^COLOR\((?<clr>\w+)\)")).Success)
+					sb.Append($"{(char)Code.COLOR}{C<Color>(m.Groups["clr"].Value)}");
+				else if ((m = Regex.Match(s, @"^HIGHSCORE\((?<hs>\w+)\)")).Success)
+					sb.Append($"{(char)Code.HIGHSCORE}" +
+						$"{C<Highscore>(m.Groups["hs"].Value)}");
+				else if (s.StartsWith(cn = "ITEM_ICON") || s.StartsWith(cn = "SHIFT") ||
+						 s.StartsWith(cn = "FADE") || s.StartsWith(cn = "TEXT_SPEED") ||
+						 s.StartsWith(cn = "BOX_BREAK_DELAYED")) {
+					m = Regex.Match(s, @$"^{cn}\(\""{hex}\""\)");
+					sb.Append($"{C<Code>(cn)}{C(m.Groups[1].Value)}");
+				}
+				else if (s.StartsWith(cn = "TEXTID") || s.StartsWith(cn = "SFX") ||
+						 s.StartsWith(cn = "FADE2")) { // FADE2 is used in STAFF
+					m = Regex.Match(s, @$"^{cn}\(\""{hex}{hex}\""\)");
+					sb.Append($"{C<Code>(cn)}" +
+						$"{C(m.Groups[1].Value)}" +
+						$"{C(m.Groups[2].Value)}");
+				}
+				else if (s.StartsWith(cn = "BACKGROUND")) {
+					m = Regex.Match(s,
+						@$"^{cn}\(\""{hex}\""\s*,\s*\""{hex}\""\s*,\s*\""{hex}\""\)");
+					sb.Append($"{(char)Code.BACKGROUND}" +
+						$"{C(m.Groups[1].Value)}" +
+						$"{C(m.Groups[2].Value)}" +
+						$"{C(m.Groups[3].Value)}");
+				}
+				else if ((m = Regex.Match(s, codes)).Success)
+					sb.Append($"{C<Code>(m.Groups["code"].Value)}");
+				if (m.Success) i += m.Length - 1;
+			}
+		}
+
+		return sb.ToString().Replace("\u201C", "\"");
+	}
+
+	public static byte[] AddEndAndAlign(byte[] data) {
+		int plusEndSize = data.Length + 1;
+		int paddingSize = (4 - (plusEndSize % 4)) % 4;
+		byte[] newData = new byte[plusEndSize + paddingSize];
+		data.CopyTo(newData, 0); // Copy old data into the new array
+		newData[data.Length] = (byte)Code.END; // Add END control code
+
+		return newData;
+	}
+
+	public static string Endec(byte[] data, bool bin, StringsDict replacements) {
+		string t = bin ? Encoding.UTF8.GetString(data) : SturmScharf.EncodingProvider.Latin1.GetString(data);
+		foreach (KeyValuePair<string, string> rep in replacements)
+			t = bin ? t.Replace(rep.Key, rep.Value) : t.Replace(rep.Value, rep.Key);
+
+		return t;
+	}
+
+	public static StringsDict LoadCharMap(string[] lines) {
+		StringsDict cm = new();
+
+		foreach (string line in lines) {
+			string[] splitLine = line.Split('=');
+			string key = splitLine[0];
+			string val = Convert.ToChar(Convert.ToUInt32(splitLine[1], 16)).ToString();
+			cm.Add(key, val);
+		}
+
+		return cm;
+	}
+
+	public static void AddSingle(this List<byte> list, MessageEntry entry) {
+		list.AddRange(Utility.ByteArray.FromU16(entry.ID, false));
+		list.Add((byte)entry.BoxType);
+		list.Add((byte)entry.BoxPos);
+		list.AddRange(Utility.ByteArray.FromI32(entry.Content!.Count, false));
+		list.AddRange(entry.Content.ToArray());
+	}
+
+	public static List<MessageEntry> Parse(string input) {
+		List<MessageEntry> textMessages = new();
+
+		// Match DEFINE_MESSAGE macro
+		string pattern = @"DEFINE_MESSAGE\((\w+),\s*(\w+),\s*(\w+),\s*([\s\S]*?)\n\s*\)";
+		Regex regex = new(pattern, RegexOptions.Singleline);
+
+		foreach (Match match in regex.Matches(input)) {
+			ushort textId = Convert.ToUInt16(match.Groups[1].Value, 16);
+			TextBoxType type = (TextBoxType)Enum.Parse(typeof(TextBoxType), match.Groups[2].Value);
+			TextBoxPosition yPos = (TextBoxPosition)Enum.Parse(typeof(TextBoxPosition), match.Groups[3].Value);
+			string messageText = EvalCodes(match.Groups[4].Value.Replace("\n", " ").Replace("\r", " "));
+			List<byte> messageData = new(AddEndAndAlign(SturmScharf.EncodingProvider.Latin1.GetBytes(messageText)));
+
+			// Add text message to list
+			textMessages.Add(new MessageEntry { ID = textId, BoxType = type, BoxPos = yPos, Content = messageData });
+		}
+
+		return textMessages;
 	}
 }
--- OTRMod/Z/Text.cs
+++ OTRMod/Z/Text.cs
@@ -7,9 +7,10 @@ using OTRMod.Utility;
 namespace OTRMod.Z;
 
 public class Text : Resource {
-	public byte[] MessageData { get; set; }
-	public byte[] TableData { get; set; }
+	public byte[]? MessageData { get; set; }
+	public byte[]? TableData { get; set; }
 	public bool AddChars { get; set; }
+	public List<MessageEntry>? Entries { get; set; }
 
 	public Text(byte[] messages, byte[] table, bool addChars) : base(ResourceType.Text) {
 		MessageData = messages;
@@ -17,75 +18,40 @@ public class Text : Resource {
 		AddChars = addChars;
 	}
 
+	public Text(byte[] messagesH, string[] charMap, bool addChars) : base(ResourceType.Text) {
+		StringsDict replacements = LoadCharMap(charMap);
+		Entries = Parse(Endec(messagesH, true, replacements));
+		AddChars = addChars;
+	}
+
+	// Used for PAL
+	private const string ToAdd = "0123456789" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+	"abcdefghijklmnopqrstuvwxyz -.";
+
 	public override byte[] Formatted() {
 		List<byte> newData = new();
-		MessageEntry entry = new();
 		int index = 0;
-		const string toAdd = "0123456789" +
-			"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-			"abcdefghijklmnopqrstuvwxyz -.";
 
-		while (index < TableData.Length) {
-			entry.ID = TableData.ToU16(index);
-			entry.BoxType = (TextBoxType)((TableData[index + 2] & 0xF0) >> 4);
-			entry.BoxPos = (TextBoxPosition)(TableData[index + 2] & 0x0F);
-			entry.Offset = TableData.ToI32(index + 4) & 0x00FFFFFF;
-			entry.Content = new();
+		if (TableData != null && MessageData != null) {
+			MessageEntry entry = new();
+			bool add;
+			do {
+				entry.ID = TableData.ToU16(index);
+				entry.BoxType = (TextBoxType)((TableData[index + 2] & 0xF0) >> 4);
+				entry.BoxPos = (TextBoxPosition)(TableData[index + 2] & 0x0F);
+				entry.Offset = TableData.ToI32(index + 4) & 0x00FFFFFF;
+				entry.Content = new();
+				add = AddMessage(newData, entry, ref index, AddChars, MessageData);
+			} while (index < TableData.Length && add);
 
-			int msg = entry.Offset;
-			byte c = MessageData[msg];
-			int extra = 0;
-			bool stop = false;
-
-			while ((c != '\0' && !stop) || extra > 0) {
-				entry.Content.Add(c);
-				msg++;
-
-				if (extra == 0) {
-					if (Enum.IsDefined(typeof(Code), c))
-						switch ((Code)c) {
-							case Code.END:
-								stop = true; break;
-							case Code.COLOR:
-							case Code.SHIFT:
-							case Code.BOX_BREAK_DELAYED:
-							case Code.FADE:
-							case Code.ITEM_ICON:
-							case Code.TEXT_SPEED:
-							case Code.HIGHSCORE:
-								extra = 1; break;
-							case Code.TEXTID:
-								extra = 2; stop = true; break;
-							case Code.FADE2:
-							case Code.SFX:
-								extra = 2; break;
-							case Code.BACKGROUND:
-								extra = 3; break;
-
-						}
-				}
-				else extra--;
-
-				c = MessageData[msg];
+		}
+		else if (Entries != null) {
+			foreach (MessageEntry entry in Entries) {
+				byte[] msgData = entry.Content!.ToArray();
+				entry.Content.Clear();
+				AddMessage(newData, entry, ref index, AddChars, msgData);
 			}
-
-			if (entry.ID is 0xFFFD)
-				if (AddChars) {
-					entry.ID = 0xFFFC;
-					entry.Content.Clear();
-					entry.Content.AddRange(System.Text.Encoding.ASCII.GetBytes(toAdd));
-				}
-				else break;
-
-			if (entry.ID is 0xFFFF) break;
-
-			newData.AddRange(ByteArray.FromU16(entry.ID, false));
-			newData.Add((byte)entry.BoxType);
-			newData.Add((byte)entry.BoxPos);
-			newData.AddRange(ByteArray.FromI32(entry.Content.Count, false));
-			newData.AddRange(entry.Content.ToArray());
-
-			index += 8;
 		}
 
 		Data = new byte[0x04 + newData.Count];
@@ -93,5 +59,59 @@ public class Text : Resource {
 		Data.Set(0x04, newData.ToArray());
 
 		return base.Formatted();
+	}
+
+	private static bool AddMessage(List<byte> msgList, MessageEntry entry, ref int index, bool addChars, byte[] msgData) {
+		int msg = entry.Offset;
+		byte c = msgData[msg];
+		int extra = 0;
+		bool stop = false;
+
+		while ((c != '\0' && !stop) || extra > 0) {
+			entry.Content!.Add(c);
+			msg++;
+
+			if (extra == 0) {
+				if (Enum.IsDefined(typeof(Code), c))
+					switch ((Code)c) {
+						case Code.END:
+							stop = true; break;
+						case Code.COLOR:
+						case Code.SHIFT:
+						case Code.BOX_BREAK_DELAYED:
+						case Code.FADE:
+						case Code.ITEM_ICON:
+						case Code.TEXT_SPEED:
+						case Code.HIGHSCORE:
+							extra = 1; break;
+						case Code.TEXTID:
+							extra = 2; stop = true; break;
+						case Code.FADE2:
+						case Code.SFX:
+							extra = 2; break;
+						case Code.BACKGROUND:
+							extra = 3; break;
+					}
+			}
+			else extra--;
+
+			if (msgData.Length > msg)
+				c = msgData[msg];
+		}
+
+		if (entry.ID is 0xFFFD)
+			if (addChars) {
+				entry.ID = 0xFFFC;
+				entry.Content!.Clear();
+				entry.Content.AddRange(System.Text.Encoding.ASCII.GetBytes(ToAdd));
+			}
+			else return false;
+
+		if (entry.ID is 0xFFFF) return false;
+
+		msgList.AddSingle(entry);
+		index += 8;
+
+		return true;
 	}
 }
