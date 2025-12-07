472c1b6f0f4bb07f78939744de161b6a7a5965d8
Amaro Martínez <xoas@airmail.cc>
2025-12-07 18:02:54 -0500

    Web: Add language selector

--- OTRMod.Web/Program.cs
+++ OTRMod.Web/Program.cs
@@ -1,8 +1,11 @@
 using BlazorDownloadFile;
+using Blazored.LocalStorage;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OTRMod.Web;
+using OTRMod.Web.Services;
+using System.Globalization;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,5 +16,19 @@ builder.Services.AddLocalization();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorDownloadFile();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<CultureService>();
 
-await builder.Build().RunAsync();
+WebAssemblyHost host = builder.Build();
+
+// Set culture from LocalStorage
+var localStorage = host.Services.GetRequiredService<ILocalStorageService>();
+var savedCulture = await localStorage.GetItemAsStringAsync(CultureService.CULTURE_KEY);
+
+if (!string.IsNullOrEmpty(savedCulture)) {
+	var culture = new CultureInfo(savedCulture);
+	CultureInfo.DefaultThreadCurrentCulture = culture;
+	CultureInfo.DefaultThreadCurrentUICulture = culture;
+}
+
+await host.RunAsync();
---
+++ OTRMod.Web/Services/CultureService.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+using Blazored.LocalStorage;
+
+namespace OTRMod.Web.Services;
+
+public class CultureService {
+	public const string CULTURE_KEY = "app-culture";
+	private readonly ILocalStorageService _localStorage;
+
+	public event Action? OnCultureChanged;
+
+	public CultureService(ILocalStorageService localStorage) {
+		_localStorage = localStorage;
+	}
+
+	public async Task<string> GetSavedCultureAsync() {
+		var culture = await _localStorage.GetItemAsStringAsync(CULTURE_KEY);
+		return culture ?? "en";
+	}
+
+	public async Task SetCultureAsync(string culture) {
+		var currentCulture = CultureInfo.CurrentCulture.Name;
+		if (currentCulture != culture) {
+			await _localStorage.SetItemAsStringAsync(CULTURE_KEY, culture);
+			
+			var newCulture = new CultureInfo(culture);
+			CultureInfo.DefaultThreadCurrentCulture = newCulture;
+			CultureInfo.DefaultThreadCurrentUICulture = newCulture;
+			
+			OnCultureChanged?.Invoke();
+		}
+	}
+}
