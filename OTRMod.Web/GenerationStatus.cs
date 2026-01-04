bfb6ad1bd5ef2d12ef0e040abbf3d4828f9df5fb
Amaro Martínez <xoas@airmail.cc>
2026-01-03 20:07:13 -0500

    Web: Wait for the script and ROM to load

--- OTRMod.Web/GenerationStatus.cs
+++ OTRMod.Web/GenerationStatus.cs
@@ -3,6 +3,7 @@ namespace OTRMod.Web;
 public enum GenerationStatus {
 	NoSelected, /* None is selected */
 	Selected,   /* At least one is selected */
+	Loading,    /* Loading files */
 	Generating, /* Processing (decompressing/creating OTR) */
 	Finished,   /* Then we're done */
 	Error,      /* F */
